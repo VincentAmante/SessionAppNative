0bd202b09251cdb5a0208826433bfe1665feea5e
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2021-12-30 13:22:58 +0400

    Redoing App.js

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import { Picker } from "@react-native-picker/picker";
+import { StyleSheet, View } from "react-native";
+
+import DayButton from "./DayButton";
+import Header from "./Header";
+import LinearGradient from "react-native-linear-gradient";
+
+const map = new Map([
+
+])
+
+export default App = () => {
+  const [availableHours, setAvailableHours] = useState(0);
+  const [availableDays, setAvailableDays] = useState(0);
+
+  const [resultHours, setResultHours] = useState(0);
+  const [resultDays, setResultDays] = useState(0)
+
+}
+
+const styles = StyleSheet.create({
+  // Containers
+  container:{
+
+  },
+  partContainer:{
+
+  },
+
+  subHeading:{
+
+  },
+
+
+  pickerText:{
+
+  }
+})
